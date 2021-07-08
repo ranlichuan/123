4a47a71d655e56b0df13a2474f2b3f2613df332c
rlc <2083636764@qq.com>
2021-07-08 20:31:08 +0800

    完善了text工具类,增加了id,name,pwd属性

--- src/com/ujiuye/entity/text.java
+++ src/com/ujiuye/entity/text.java
@@ -1,4 +1,40 @@
 package com.ujiuye.entity;
 
 public class text {
+    private int id;
+    private String name;
+    private int pwd;
+
+    public text() {
+    }
+
+    public text(int id, String name, int pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(int pwd) {
+        this.pwd = pwd;
+    }
 }
